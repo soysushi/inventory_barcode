ea89d2dd861f6019b3038ec4468eab24070a5e2f
soysushi <chi_oscar@hotmail.com>
2021-12-16 17:21:27 -0800

    label-some-bugs

--- store/views.py
+++ store/views.py
@@ -31,7 +31,8 @@ from .forms import (
 )
 
 from .helpers import (
-    generate_label
+    generate_label,
+    print_label
 )
 
 # Supplier views
@@ -194,8 +195,11 @@ def create_product(request):
 @login_required(login_url='login')
 def product_list(request):
     if request.method == 'POST':
-        items = request.POST.getlist("product")
-        print(items)
+        barcodes = request.POST.getlist("product")
+        items = []
+        for barcode in barcodes:
+            items = Product.objects.filter(sortno=barcode)
+        print_label(items)
     products = Product.objects.all()
     for product in products:
          variants = product.productvariant_set.all()
